fee1c6ac9de29afedc29eb906996d1ec09debab6
LCap <banlazthosow@gmail.com>
2022-11-06 14:25:25 +0200

    commit

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { Button } from "../../components/Button/Button";
@@ -7,9 +7,20 @@ import { Input } from "../../components/Input/Input";
 import "./LogIn.scss";
 
 export const LogIn = () => {
+  let [err, setErr] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    console.log(`email: ${email}\npassword: ${password}`);
+  };
+
   return (
     <div className="log-in">
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <h2>authorization</h2>
         <Input type={"email"} placeholder={"Email"} />
         <Input type={"password"} placeholder={"Password"} />
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "../../components/Button/Button";
 import { Input } from "../../components/Input/Input";
 import "./SignIn.scss";
 
 export const SignIn = () => {
+  let [err, setErr] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+
+    console.log(`name: ${name}\nemail: ${email}\npassword: ${password}`);
+  };
+
   return (
     <div className="sign-in">
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <h2>Registration</h2>
         <Input type={"text"} placeholder={"Name"} />
         <Input type={"email"} placeholder={"Email"} />
