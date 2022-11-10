f2b9e8834d53cdaa25c8d85e5a9d5d0e6bffa36d
LCap <banlazthosow@gmail.com>
2022-11-10 16:02:25 +0200

    commit

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -11,6 +11,12 @@ export const Posts = () => {
           "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At vel quo ab sunt suscipit placeat commodi eligendi, dolor sint natus sit omnis fuga sapiente, tenetur aspernatur fugiat doloremque repellendus consequatur."
         }
       />
+      <Post
+        title={"hi"}
+        description={
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At vel quo ab sunt suscipit placeat commodi eligendi, dolor sint natus sit omnis fuga sapiente, tenetur aspernatur fugiat doloremque repellendus consequatur."
+        }
+      />
     </div>
   );
 };
